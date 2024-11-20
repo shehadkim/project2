7eca7a4a3a9052a4bd73430be1d5d25caf6c1194
shehadkim <shehadkim@gmail.com>
2024-11-20 18:23:42 +0300

    Add files via upload

---
+++ js/honey.js
@@ -0,0 +1,7 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="50522683-4bb9-4847-b814-b6f88c6c08af";
+(function(){ d=document;s=d
+.createElement("script"); s.src="https://client.crisp.chat/l.js"; 
+s.async=1;d.getElementsByTagName("head")
+[0].appendChild(s);})(); 
+
+
